29eae9ee1755a4982055d42b34f8e14f8b51b156
Finn1510 <49493608+Finn1510@users.noreply.github.com>
2024-04-24 15:21:42 +0200

    better config

--- main.cpp
+++ main.cpp
@@ -16,11 +16,14 @@ int main(int argc, char* args[]) {
 
     sim.celestialBodies.push_back(std::make_shared<Sun>(90000000000000000, center, Vector2(0,0), 25)); //add a sun at the center
     const SDL_Color planetColor1 = {129, 192, 160, 255};
-    sim.celestialBodies.push_back(std::make_shared<Planet>(500, center + Vector2(0, -300), Vector2(-100, 0), planetColor1, 12));
+    sim.celestialBodies.push_back(std::make_shared<Planet>(900, center - Vector2(SCREEN_WIDTH/4, 0), Vector2(0, -150/2), planetColor1, 12));
     
     const SDL_Color planetColor2 = {161, 119, 86, 255};
-    sim.celestialBodies.push_back(std::make_shared<Planet>(400, center + Vector2(0, 100), Vector2(200, 0), planetColor2, 10));
+    sim.celestialBodies.push_back(std::make_shared<Planet>(400, center + Vector2(0, 300), Vector2(-135/2, 0), planetColor2, 10));
     
+    const SDL_Color planetColor3 = {100, 119, 186, 255};
+    sim.celestialBodies.push_back(std::make_shared<Planet>(25, center + Vector2(SCREEN_WIDTH/4, 0), Vector2(0, 120/2), planetColor3, 8));
+
     //Simulation loop
     while(true){
         sim.SimulationStep();
